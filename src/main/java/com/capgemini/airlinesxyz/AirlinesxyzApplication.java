a37bf00051c241401bdd024fa59ae878cafd187c
ashjaminhas <38127720+ashjaminhas@users.noreply.github.com>
2018-05-15 09:39:01 +0200

    Commit
    Commit

---
+++ src/main/java/com/capgemini/airlinesxyz/AirlinesxyzApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.airlinesxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesxyzApplication.class, args);
+	}
+}
